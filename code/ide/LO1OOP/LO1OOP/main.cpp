647e4ed30f7a38792b3b1a993279e7377036e735
zhixuan183720950 <lingjianfeng@calendar.google.com127.0.0.1>
2014-09-27 14:48:18 +0800

    Initial Commit

---
+++ code/ide/LO1OOP/LO1OOP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LO1OOP
+//
+//  Created by Lance on 14-9-27.
+//  Copyright (c) 2014年 com.babybus.cn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
