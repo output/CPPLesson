af144f6fed69dca15ebd8fb7fcf56f10a9b22144
zhixuan183720950 <lingjianfeng@calendar.google.com127.0.0.1>
2014-09-27 15:17:29 +0800

    Person 类

---
+++ code/ide/LO1OOP/LO1OOP/Person.cpp
@@ -0,0 +1,13 @@
+//
+//  Person.cpp
+//  LO1OOP
+//
+//  Created by Lance on 14-9-27.
+//  Copyright (c) 2014年 cn.babybus.com. All rights reserved.
+//
+
+#include "Person.h"
+
+void Person::sayHello(){
+    printf("Hello CPP\n");
+}
--- code/ide/LO1OOP/LO1OOP/main.cpp
+++ code/ide/LO1OOP/LO1OOP/main.cpp
@@ -3,16 +3,22 @@
 //  LO1OOP
 //
 //  Created by Lance on 14-9-27.
-//  Copyright (c) 2014年 com.babybus.cn. All rights reserved.
+//  Copyright (c) 2014年 cn.babybus.com. All rights reserved.
 //
 
 #include <iostream>
 
+#include "Person.h"
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Person *p = new Person();
+    p->sayHello();
+    
+    // 删除对象，内存回收 ， new 和 delete 成对出现
+    delete p;
+    
     return 0;
 }
 
